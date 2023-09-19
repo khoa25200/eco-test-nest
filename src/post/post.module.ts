63beed92e359aacbd249615e8eae57083ef94f85
khoa25200 <ngdakhoa@gmail.com>
2023-09-19 11:07:41 +0700

    auth & refresh token & handle exceptions

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -4,6 +4,11 @@ import { PostService } from './services/post.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostSchema } from './models/post.model';
 import { PostRepository } from './repositories/post.repository';
+import { UserModule } from '../user/user.module';
+import { CategorySchema } from './models/category.model';
+import { CategoryController } from './controllers/category.controller';
+import { CategoryRepository } from './repositories/category.repository';
+import { CategoryService } from './services/category.service';
 
 @Module({
   imports: [
@@ -12,9 +17,14 @@ import { PostRepository } from './repositories/post.repository';
         name: 'Post',
         schema: PostSchema,
       },
+      {
+        name: 'Category',
+        schema: CategorySchema,
+      },
     ]),
+    UserModule,
   ],
-  controllers: [PostController],
-  providers: [PostService, PostRepository],
+  controllers: [PostController, CategoryController],
+  providers: [PostService, PostRepository, CategoryRepository, CategoryService],
 })
 export class PostModule {}
